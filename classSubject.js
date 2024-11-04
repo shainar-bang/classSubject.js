b7fe7bdf2fa1a591833665dad55a4787c5664d67
shainar-bang <rabangshaina@gmail.com>
2024-11-04 18:36:25 +0800

    Create classSubject.js

---
+++ classSubject.js
@@ -0,0 +1,7 @@
+let subjectTitle = "Data Structures and Algorithms"
+let classSchedule = "9:30-10:30 in Monday and Friday"
+let classroom = "Computer Laboratory A"
+let classInstructor = "Mr. Vitruvius John Barayuga"
+let studentName = "Shaina Mae P. Rabang"
+
+console.log(`${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}.`)
